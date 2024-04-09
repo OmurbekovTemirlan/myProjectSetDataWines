9660bde318aa49b877761957df4af234409d5f1a
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-04-09 19:45:52 +0600

    after images

---
+++ myProject/Views/Home/HomeController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeController.swift
+//  myProject
+//
+//  Created by Apple on 9.4.2024.
+//
+
+import Foundation
---
+++ myProject/Views/Home/HomeModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModel.swift
+//  myProject
+//
+//  Created by Apple on 9.4.2024.
+//
+
+import Foundation
