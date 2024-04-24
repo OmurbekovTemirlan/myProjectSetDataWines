7c7368a2888fffbb09a6dc94c0329094e4418845
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-04-24 19:17:07 +0600

    last

--- myProject/Views/Settings/Thems/ThemsCell.swift
+++ myProject/Views/Settings/Thems/ThemsCell.swift
@@ -5,4 +5,53 @@
 //  Created by Apple on 16.4.2024.
 //
 
-import Foundation
+import UIKit
+
+struct ThemsStruct {
+    var title: String
+}
+
+class ThemsCell: UITableViewCell {
+    
+    static let idCell = "IDCELL" 
+    
+    private lazy var titleLabel: UILabel = {
+        let view = UILabel()
+        view.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var switchControlThems: UISwitch = {
+        let view = UISwitch()
+        view.contentHorizontalAlignment = .left
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+    
+    private func setupUI(){
+        addSubview(titleLabel)
+        addSubview(switchControlThems)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+            switchControlThems.centerYAnchor.constraint(equalTo: centerYAnchor),
+            switchControlThems.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5)
+        ])
+    }
+    func set(title: ThemsStruct){
+        titleLabel.text = title.title
+    }
+}
--- myProject/Views/Settings/Thems/ThemsView.swift
+++ myProject/Views/Settings/Thems/ThemsView.swift
@@ -9,53 +9,45 @@ import UIKit
 
 class ThemsView: UIViewController {
     
-    private lazy var hStac: UIStackView = {
-        let view = UIStackView()
-        view.axis = .horizontal
-        view.distribution = .fill
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.alignment = .center
-        view.backgroundColor = .systemGray6
-        view.layer.cornerRadius = 13
-        view.layer.borderWidth = 1
-        view.layer.borderColor = UIColor.darkGray.cgColor
-        return view
-    }()
+    private var them: [ThemsStruct] = [ThemsStruct(title: "Dark them")]
     
-    private lazy var themsLabel: UILabel = {
-        let view = UILabel()
-        view.text = "   Темная тема"
-        view.isHidden = false
-        view.textAlignment = .left
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    private lazy var switchControlThems: UISwitch = {
-        let view = UISwitch()
-        view.contentHorizontalAlignment = .left
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    private lazy var themsTableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupUI()
     }
-    
     private func setupUI(){
-        view.addSubview(hStac)
-        hStac.addArrangedSubview(themsLabel)
-        hStac.addArrangedSubview(switchControlThems)
-        
+        view.addSubview(themsTableView)
         NSLayoutConstraint.activate([
-            hStac.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            hStac.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            hStac.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            hStac.heightAnchor.constraint(equalToConstant: 50),
-            
-            switchControlThems.trailingAnchor.constraint(equalTo: hStac.trailingAnchor, constant: -10),
+            themsTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            themsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            themsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
         ])
+        themsTableView.translatesAutoresizingMaskIntoConstraints = false
+        themsTableView.dataSource = self
+        themsTableView.register(ThemsCell.self, forCellReuseIdentifier: ThemsCell.idCell)
+//        themsTableView.delegate = self
+        themsTableView.backgroundColor = .blue
+    }
+    
+}
+
+extension ThemsView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        them.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ThemsCell.idCell, for: indexPath) as! ThemsCell
+        cell.set(title: them[indexPath.row])
+        cell.backgroundColor = .systemGray4
+        cell.layer.cornerRadius = 14
+        return cell
     }
 }
+//
+//extension ThemsView: UITableViewDelegate{
+//    
+//}
